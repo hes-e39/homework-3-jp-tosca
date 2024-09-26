b42302faccb725233f393a95cd9a97b49aa3dc01
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-09-26 13:31:49 -0400

    Colors display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import colors from './colors.json';
+
+type Color = {
+    name: string;
+    hex: string;
+};
+
+const ColorRow = ({ color }: { color: Color }) => {
+    return (
+        <tr style={{ color: `#${color.hex}` }}>
+            <td>{color.name}</td>
+            <td>{color.hex}</td>
+        </tr>
+    );
+};
+
+const ColorsDisplay = () => {
+    return (
+        <div>
+            <ColorsInfoDisplay />
+            <table className="table-auto mx-auto">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Hex</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {colors.map(color => (
+                        <ColorRow key={color.hex} color={color} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+const ColorsInfoDisplay = () => {
+    return <h1 className="text-4xl text-center">Colors on file: {colors.length}</h1>;
+};
+
 const App = () => {
-    return <h1>Change test!</h1>;
+    return <ColorsDisplay />;
 };
 
 export default App;
